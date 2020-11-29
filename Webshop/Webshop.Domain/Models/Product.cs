34e431fdc16a4171e4f75bd0a2441cd7738a7b6b
Wesley Dierckx <dierckxwesley@outlook.com>
2020-11-29 11:47:48 +0100

    Cleaned up Architecture Added Product to domain

---
+++ Webshop/Webshop.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Webshop.Domain.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string DescriptionShort { get; set; }
+        public string DescriptionLong { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
