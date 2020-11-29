e3399cd6b8175d3d73a65852ea35ff388db090a2
Wesley Dierckx <dierckxwesley@outlook.com>
2020-11-29 15:38:49 +0100

    Added ProductFormViewModel Added ProductForm

--- Webshop/Webshop/Controllers/ProductsController.cs
+++ Webshop/Webshop/Controllers/ProductsController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ViewModels.Product;
 
 namespace Webshop.Controllers
 {
     public class ProductsController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult NewProduct()
+        {
+            var viewModel = new ProductFormViewModel();
+
+            return View("ProductForm", viewModel);
         }
     }
 }
