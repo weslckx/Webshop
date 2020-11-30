69f0ecc9864da398c0e06f5350dbd8ff138d4d87
Wesley Dierckx <dierckxwesley@outlook.com>
2020-11-30 21:28:06 +0100

    Added basic Productconfig (fluent api)

---
+++ Webshop/Webshop.Data/Persistence/EntityConfigurations/ProductConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Models;
+
+namespace Webshop.Data.Persistence.EntityConfigurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        // Not in constructor. Added to ModelBuilder using ApplyConfiguration in ShopDbContext
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder
+                .Property(t => t.Id)
+                .IsRequired();
+            builder
+                .Property(t => t.Name)
+                .HasColumnName("Productnaam")
+                .HasMaxLength(100)
+                .IsRequired();
+            builder
+                .Property(t => t.DescriptionShort)
+                .HasColumnName("Korte omschrijving")
+                .HasMaxLength(550)
+                .IsRequired();
+            builder
+                .Property(t => t.Price)
+                .HasColumnName("Prijs")
+                .HasColumnType("decimal(5,2)")
+                .IsRequired();
+            builder
+                .Property(t => t.DescriptionLong)
+                .HasColumnName("Omschrijving");
+            builder
+                .Property(t => t.ImageUrl)
+                .HasColumnName("Link naar afbeelding");
+
+
+            
+            
+
+        }
+    }
+}
--- Webshop/Webshop.Data/ShopDbContext.cs
+++ Webshop/Webshop.Data/ShopDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Webshop.Data.Persistence.EntityConfigurations;
 using Webshop.Domain.Models;
 
 namespace Webshop.Data
@@ -14,7 +15,20 @@ namespace Webshop.Data
         {
         }
 
-        //public DbSet<Product> Products { get; set; }
-        public DbSet<Product> Products { get; set; }
+      
+        public virtual DbSet<Product> Products { get; set; }
+
+
+        //Old way: DBcontext uses his method OnModelCreating with param ModelBuilder. Is called when context is first created
+        // It is possible to override this.
+        // https://www.learnentityframeworkcore.com/configuration/fluent-api 
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProductConfiguration());
+        }
+
     }
+
+
 }
