a8f33e811fd438feb0e5ba903bc32eb154ac4c0e
Wesley Dierckx <dierckxwesley@outlook.com>
2020-12-15 18:43:46 +0100

    Added WebshopUser and Customer Ran into issue clean architecture vs Identity: need to resolve!

---
+++ Webshop/Webshop.Domain/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Webshop.Domain.Models
+{
+    public class Customer
+    {
+
+        public int CustomerId { get; set; }
+
+        public string WebshopUserId { get; set; }
+        //public WebShopUser WebshopUser { get; set; } 
+        // Hmm, om dit toe te voegen reference nodig naar mijn Data
+        // zo opvragen door toe te voegen aan order? Valt nog af te wachten
+        //
+
+        //https://stackoverflow.com/questions/63956730/user-entity-in-clean-architecture
+
+    }
+}
--- Webshop/Webshop/Areas/Identity/Data/WebshopUser.cs
+++ Webshop/Webshop/Areas/Identity/Data/WebshopUser.cs
@@ -3,16 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Webshop.Domain.Models;
 
 namespace Webshop.Areas.Identity.Data
 {
+    [PersonalData]
     public class WebshopUser: IdentityUser
     {
         public string Name { get; set; }
-        public string FirstName { get; set; }
-        public string Address { get; set; }
-        public int ZipCode { get; set; }
-        public string City { get; set; }
 
+        public string FirstName { get; set; }
+
+        public string Address { get; set; }
+
+        public string ZipCode { get; set; }
+  
+        public string City { get; set; }
     }
 }
