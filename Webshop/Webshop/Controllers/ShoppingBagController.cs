40b7e31dde1bbb9668f7d0033ae34babd4cb5d55
Wesley Dierckx <dierckxwesley@outlook.com>
2020-12-19 18:56:08 +0100

    Added cookie config Added cookie policyconfig & view

--- Webshop/Webshop/Controllers/ShoppingBagController.cs
+++ Webshop/Webshop/Controllers/ShoppingBagController.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Webshop.Controllers
 {
     public class ShoppingBagController : Controller
     {
+        
+
         public IActionResult Index()
         {
+            ViewData["id"] = HttpContext.Session.Id;
+
             return View();
         }
     }
