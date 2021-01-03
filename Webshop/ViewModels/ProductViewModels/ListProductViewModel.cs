24064ba11c5f205d7b84d5f0218aadcbe7cb5fdb
Wesley Dierckx <dierckxwesley@outlook.com>
2021-01-03 16:35:20 +0100

    Added ListProductViewModel

---
+++ Webshop/ViewModels/ProductViewModels/ListProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Models;
+
+namespace ViewModels
+{
+    public class ListProductViewModel
+    {
+        public string ProductSearch { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
