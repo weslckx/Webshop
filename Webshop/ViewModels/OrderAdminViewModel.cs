7dd027dd8a3eec6af562447c124c4e6db3dea635
Wesley Dierckx <dierckxwesley@outlook.com>
2021-01-05 12:50:51 +0100

    Added Dashboard ViewOrders

---
+++ Webshop/ViewModels/OrderAdminViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Models;
+
+namespace ViewModels
+{
+    public class OrderAdminViewModel
+    {
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
--- Webshop/Webshop/Controllers/AdminController.cs
+++ Webshop/Webshop/Controllers/AdminController.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ViewModels;
+using Webshop.Data.Repositories;
 
 namespace Webshop.Controllers
 {
     public class AdminController : Controller
     {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AdminController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult ViewOrders()
+        public async Task<IActionResult> ViewOrders()
         {
-            return View();
+            var orders = await _unitOfWork.Orders.GetAll();
+            OrderAdminViewModel viewModel = new OrderAdminViewModel
+            {
+                Orders = orders
+            };
+
+
+            return View(viewModel);
         }
     }
 }
